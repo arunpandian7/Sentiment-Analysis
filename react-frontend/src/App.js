85e23b3f4b5284bea240b7e1b61cfb00b09462cb
Arun Pandian R <smartboyarun56@gmail.com>
2020-05-17 22:37:26 +0530

    Added Search Bar

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,16 +1,89 @@
 import React from 'react';
 
-
+import { Typography } from '@material-ui/core';
 import Styles from './App.module.css';
-import {SearchBar, Chart, Result} from './Components';
+import {Chart, Result} from './Components';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
+
+const axios = require('axios');
 
 class App extends React.Component{
+
+  state = {
+    hashtagText : "",
+    progressBar : false,
+    submitted : false,
+    options:{
+      
+    },
+    hashtagDesc : "",
+  }
+
+  clickHandler = () => {
+    this.setState({progressBar: true});
+    this.setState({submitted: false});
+    var postive = 0
+    var negative = 0
+    var neutral = 0
+    var self = this;
+    try{
+      axios.get('http://localhost:8000/analysehastag',{
+        params:{
+          text: this.state.hashtagDesc
+        }
+      }).then(function(response){
+        negative = response.data.negative
+        postive = response.data.postive
+        neutral = response.data.neutral
+        self.setState({submitted: true});
+        self.setState({series:[negative, postive, neutral]});
+      }, 3000);
+    } catch(e){
+      console.log(e);
+    }
+    
+    try {        
+      var url = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + this.state.hashtag + "&limit=1&format=json"
+        axios.get(url).then(function(response) {
+            self.setState({hashtag_desc: response.data[2][0]});
+        });
+        } catch(e) {
+          console.log(e);
+        }
+  }
+
+  textHandler = (event) =>{
+    this.setState({hashtagText: event.target.value})
+  };
+
+
+  
+  
   render(){
+    
     return (
       <div className={Styles.container}>
-        <h1 className="display-4 text-center"> Sentiment Analysis App</h1>
-        <br /><br />
-        <SearchBar />
+
+        <Typography variant='h2' color='primary'>Sentiment Analysis</Typography>
+        <Paper component="form" rounded elevation={7} style={{padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 600,
+        borderRadius: 30,}}>
+          <InputBase
+            style={{marginLeft: 10,
+              flex: 1}}
+            placeholder="Search"
+            inputProps={{ 'aria-label': 'search' }}
+            onChange = {this.textHandler}
+          />
+        </Paper>
+        <Button className={Styles.analyseButton} variant="outlined" color="primary" onClick={clickHandler}>
+            Primary
+        </Button>
         <Chart />
         <Result />
       </div>
--- react-frontend/src/Components/index.js
+++ react-frontend/src/Components/index.js
@@ -1,3 +1,2 @@
 export {default as Chart} from './Chart/Chart';
-export {default as SearchBar} from './SearchBar/SearchBar';
 export {default as Result} from './Result/Result';
