2ea219195d75f58c412be3dded3a9134324f1d47
Arun Pandian R <smartboyarun56@gmail.com>
2020-05-22 17:25:30 +0530

    ADDED Tweet Cards

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,156 +1,206 @@
-import React from 'react';
-import Chart from 'react-apexcharts'
-import { Typography } from '@material-ui/core';
-import Styles from './App.module.css';
-import InputBase from '@material-ui/core/InputBase';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
 
-const axios = require('axios');
-class App extends React.Component{
-
+import Chart from "react-apexcharts";
+import { Typography } from "@material-ui/core";
+import Styles from "./App.module.css";
+import InputBase from "@material-ui/core/InputBase";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import axios from "axios";
+import TweetCard from "./TweetCard";
+class App extends React.Component {
   state = {
-    hashtagText : "",
-    progressBar : false,
-    submitted : false,
-    hashtagDesc : "",
+    hashtagText: "",
+    progressBar: false,
+    submitted: false,
+    hashtagDesc: "",
+    tweets : [],
     series: [44, 55, 41],
     options: {
-      colors: ['#F7464A', '#46BFBD', '#FDB45C'],
-      labels: ['Positive', 'Negative', 'Neutral'],
+      colors: ["#F7464A", "#46BFBD", "#FDB45C"],
+      labels: ["Positive", "Negative", "Neutral"],
       plotOptions: {
-      pie: {
-        donut: {
-          labels: {
-            show: true
-          }
-        }
-      }
-    }
-  },
-  }
-
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+            },
+          },
+        },
+      },
+    },
+  };
 
   // async componentDidUpdate() {
   //     var self = this;
-  //     try {        
+  //     var positive = 0;
+  //     var negative = 0;
+  //     var neutral = 0;
+  //     try {
   //       setInterval(async () => {
   //       axios.get('http://localhost:8000/analyzehashtag', {
   //           params: {
   //               text: this.state.hashtagText
   //           }
   //       }).then(function(response) {
-  //           self.setState({progressBar: false})
-  //           self.setState({submitted: true});
-  //           self.setState({seriesData: response.data});
+  //         positive = response.data.positive;
+  //         negative = response.data.negative;
+  //         neutral = response.data.neutural;
+  //         self.setState({ submitted: true });
+  //         self.setState({ progressBar: false });
+  //         self.setState({ series: [positive, negative, neutral] });
   //       });
   //           }, 3000);
   //       } catch(e) {
   //         console.log(e);
   //       }
-      
-      
+
   //   }
 
   clickHandler = () => {
-    console.log("Sending GET Request....")
+    console.log("Sending GET Request....");
     console.log(this.state.hashtagText);
     var positive = 0;
     var negative = 0;
     var neutral = 0;
 
-    this.setState({progressBar: true});
-    this.setState({submitted: false});
+    this.setState({ progressBar: true });
+    this.setState({ submitted: false });
     var self = this;
-    try{
-      axios.get('http://localhost:8000/analyzehashtag',{
-        params:{
-          text: this.state.hashtagText
-        }
-      }).then(function(response){
-        console.log("Got Response:");
-        console.log(response.data);
-        positive = response.data.positive;
-        negative = response.data.negative;
-        neutral = response.data.neutural;
-        self.setState({submitted: true});
-        self.setState({progressBar: false});
-        self.setState({series: [positive, negative, neutral]});
-      });
-    } catch(e){
+    try {
+      axios
+        .get("http://localhost:8000/analyzehashtag", {
+          params: {
+            text: this.state.hashtagText,
+          },
+        })
+        .then(function (response) {
+          console.log("Fetched Sentiment Analysis Data...");
+          console.log(response.data);
+          positive = response.data.positive;
+          negative = response.data.negative;
+          neutral = response.data.neutural;
+          self.setState({ submitted: true });
+          self.setState({ progressBar: false });
+          self.setState({ series: [positive, negative, neutral] });
+        });
+    } catch (e) {
       console.log(e);
     }
-    
+
+    try {
+      var url =
+        "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" +
+        this.state.hashtagText  +
+        "&limit=1&format=json";
+      axios.get(url).then(function (response) {
+        console.log("Wikipedia Replied");
+        console.log(response.data);
+        self.setState({ hashtag_desc: response.data[2][0] });
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
     try {        
-      var url = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=" + this.state.hashtag + "&limit=1&format=json"
-        axios.get(url).then(function(response) {
-            self.setState({hashtag_desc: response.data[2][0]});
-        });
-        } catch(e) {
-          console.log(e);
-        }
-  }
+      setInterval(async () => {
+      axios.get('http://localhost:8000/fetchTweets', {
+          params: {
+              text: this.state.hashtagText
+          }
+      }).then(function(response) {
+          // console.log(response);
+          console.log("Fetched Tweets...")
+          self.setState({tweets: response.data.results});
+      });
+          }, 30000);
+      } catch(e) {
+        console.log(e);
+      }
 
-  showLoadingBar = () => {
-    if(this.state.progressBar){
-        return(
-            <div className="text-center">
-                <img src={require('./loading.gif')} width="200" alt='loading-gif'/>
-                <h2 className="progressheader">Please Wait</h2>
-            </div>
-        );
-    }
-}
-
-
-  showChart = () =>{
-    if(this.state.submitted === true){
-      return(
-        <div>
-          
-          <Chart options={this.state.options} series={this.state.series} type="donut" width="420" />
-          <h1 class="heading_desc">{this.state.hashtag_desc}</h1>
-
-        </div>
-      )
-    }
-  }
-
-  textHandler = (event) =>{
-    this.setState({hashtagText: event.target.value})
   };
 
+  showLoadingBar = () => {
+    if (this.state.progressBar) {
+      return (
+        <div className="text-center">
+          <img src={require("./loading.gif")} width="200" alt="loading-gif" />
+          <h2 className="progressheader">Please Wait</h2>
+        </div>
+      );
+    }
+  };
 
-  
-  
-  render(){
-    
+  showChart = () => {
+    if (this.state.submitted === true) {
+      return (
+        <div>
+          <Chart
+            options={this.state.options}
+            series={this.state.series}
+            type="donut"
+            width="420"
+          />
+          <h1 class="heading_desc">{this.state.hashtag_desc}</h1>
+        </div>
+      );
+    }
+  };
+
+  textHandler = (event) => {
+    this.setState({ hashtagText: event.target.value });
+  };
+
+  render() {
+
+
+      
+    var renderTweets = this.state.tweets.map(function(item, i){
+          return (
+                <TweetCard data={{item, i}} />
+              );
+        })
+
+
+
+
     return (
       <div className={Styles.container}>
-
-        <Typography variant='h2' color='primary'>Sentiment Analysis</Typography>
-        <Paper component="form" rounded elevation={7} style={{padding: '2px 4px',
-        display: 'flex',
-        alignItems: 'center',
-        width: 600,
-        borderRadius: 30,}}>
+        <Typography variant="h2" color="primary">
+          Sentiment Analysis
+        </Typography>
+        <Paper
+          component="form"
+          rounded
+          elevation={7}
+          style={{
+            padding: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 600,
+            borderRadius: 30,
+          }}
+        >
           <InputBase
-            style={{marginLeft: 10,
-              flex: 1}}
+            style={{ marginLeft: 10, flex: 1 }}
             placeholder="Search"
-            inputProps={{ 'aria-label': 'search' }}
-            onChange = {this.textHandler}
+            inputProps={{ "aria-label": "search" }}
+            onChange={this.textHandler}
           />
         </Paper>
-        <Button className={Styles.analyseButton} variant="outlined" color="primary" onClick={this.clickHandler}>
-            Analyze
-        </Button>
+        <Button
+          className={Styles.analyseButton}
+          variant="outlined"
+          color="primary"
+          onClick={this.clickHandler}
+        >
+          Analyze
+        </ Button>
         {this.showChart()}
+        {this.state.submitted?renderTweets:<br />}
         {this.showLoadingBar()}
       </div>
-
-    )
-    
+    );
   }
 }
 
---
+++ react-frontend/src/TweetCard.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import {Card, CardContent} from '@material-ui/core';
+import {Typography} from '@material-ui/core';
+
+
+
+function TweetCard(props){
+    var color = "#46BFBD";
+    console.log(props.data)
+    if(props.data.item.label === "Neutral"){
+        color = "#FDB45C";
+    }
+    if(props.data.item.label === "Negative"){
+        color = "#F7464A";
+    }
+
+    return(
+        <div key={props.data.i}>
+        <Card variant="outlined">
+            <CardContent>
+                <Typography 
+                variant="h4"
+                color = "textSecondary"
+                gutterBottom
+                >
+                    @{props.data.item.userid}
+                </Typography>
+
+                <Typography
+                variant = "body2"
+                >
+                    {props.data.item.tweet}
+                </Typography>
+
+                <Typography
+                variant = "h5"
+                style={{"color": color}}
+                >
+                    Sentiment Predicted : {props.data.item.label}
+
+                </Typography>
+                
+            </CardContent>
+
+        </Card>
+        </div>
+    )
+
+}
+
+
+export default TweetCard;
