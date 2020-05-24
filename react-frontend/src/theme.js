96b8d44994d08ed320df938f3197bea927ca9379
Arun Pandian R <smartboyarun56@gmail.com>
2020-05-24 22:54:27 +0530

    UPDATED Theme

--- react-frontend/src/theme.js
+++ react-frontend/src/theme.js
@@ -17,7 +17,41 @@ const theme = createMuiTheme({
         contrastText: '#000',
       },
     },
+  
   });
 
+theme.typography.h2 = {
+  fontSize:'2.0rem',
+  [theme.breakpoints.up('md')]:{
+    fontSize:'3.4rem',
+    letterSpacing:'0.9rem'
+  },
+  letterSpacing:"0.1rem"
+}
 
+theme.typography.h4 ={
+  fontSize:'1.4rem',
+  [theme.breakpoints.up('md')]:{
+    fontSize:'2.4rem',
+    letterSpacing: '0.3rem'
+  },
+  letterSpacing:'0.09rem'
+}
+
+theme.typography.body1 ={
+  fontSize:'0.8rem',
+  [theme.breakpoints.up('md')]:{
+    fontSize:'1.2rem',
+    letterSpacing:'0.09rem',
+    lineHeight:'2.2rem'
+  },
+  lineHeight:'1.8rem'
+}
+
+theme.typography.caption ={
+  lineSpacing: '2rem',
+  [theme.breakpoints.up('md')]:{
+    letterSpacing:'0.2rem'
+  },
+}
 export default theme;
